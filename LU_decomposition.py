b59d729042392f3bbc1fe4794274774731817a0a
MaoSong2022 <maosong@pjlab.org.cn>
2023-02-17 18:34:54 +0800

    ✨ feat(lu_decompositions): add gaussian LU methods and tests

--- LU_decomposition.py
+++ LU_decomposition.py
@@ -2,6 +2,28 @@ import numpy as np
 from typing import Tuple
 
 
+def gaussian_lu(A: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
+    m, n = A.shape
+
+    if m != n:
+        raise ValueError("LU decomposition is only valid for square matrix.")
+    
+    L = np.eye(n)
+    I = np.eye(n)
+    for k in range(n):
+        if abs(A[k, k]) <= 1e-8:
+            raise ZeroDivisionError("pivot should not be zero")
+        tau = np.zeros((n, 1))
+        tau[k+1:, 0] = A[k+1:, k] / A[k, k]
+        # print(tau.shape)
+        # print(I[:, k:k+1].T.shape)
+        # print((tau @ I[:, k:k+1].T).shape)
+        M = np.eye(n) - tau @ I[:, k:k+1].T
+        A = M @ A
+        L += tau @ I[:, k:k+1].T
+
+    return (L, A)
+
 def out_product_lu(A: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     """use the out product to compute the LU decomposition of matrix A
 
--- LU_decomposition_test.py
+++ LU_decomposition_test.py
@@ -2,7 +2,7 @@ import scipy.linalg as la
 import unittest
 import numpy as np
 
-from LU_decomposition import out_product_lu
+from LU_decomposition import out_product_lu, gaussian_lu
 
 class TestLUDecomposition(unittest.TestCase):
     def setUp(self):
@@ -12,18 +12,30 @@ class TestLUDecomposition(unittest.TestCase):
                            [-114.65834707,    0.84312809,   22.4641862,   -53.75328591, -152.18389341],
                            [ -71.75615361,  -80.83956885, -166.6450018,   142.40901914,   74.17852845]])
 
-    def test1(self):
+    def test_out_product1(self):
         B = self.A.copy()
         test_L, test_U = out_product_lu(B)
         np.testing.assert_array_almost_equal(test_L @ test_U, self.A, decimal=3)
 
-    def test2(self):
-        P, real_L, real_U = la.lu(self.A)
-        test_L, test_U = out_product_lu(np.dot(P.T, self.A))
+    def test_out_product2(self):
+        B = self.A.copy()
+        P, real_L, real_U = la.lu(B)
+        test_L, test_U = out_product_lu(np.dot(P.T, B))
         np.testing.assert_array_almost_equal(real_L, test_L, decimal=3)
         np.testing.assert_array_almost_equal(real_U, test_U, decimal=3)
 
 
+    def test_gaussian1(self):
+        B = self.A.copy()
+        test_L, test_U = gaussian_lu(B)
+        np.testing.assert_array_almost_equal(test_L @ test_U, self.A, decimal=3)
+
+    def test_gaussian2(self):
+        B = self.A.copy()
+        P, real_L, real_U = la.lu(self.A)
+        test_L, test_U = gaussian_lu(np.dot(P.T, B))
+        np.testing.assert_array_almost_equal(real_L, test_L, decimal=3)
+        np.testing.assert_array_almost_equal(real_U, test_U, decimal=3)
 
 if __name__ == '__main__':
     unittest.main()
