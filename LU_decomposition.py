2df1ce1f005f460d34d62876a75295f0f7d31a4c
MaoSong2022 <maosong@pjlab.org.cn>
2023-02-17 10:03:02 +0800

    🐞 fix(LU decomposition): fix the index error

--- LU_decomposition.py
+++ LU_decomposition.py
@@ -18,17 +18,16 @@ def out_product_lu(A: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     if m != n:
         raise ValueError("LU decomposition is only valid for square matrix.")
 
-    if n == 1:
-        return (np.ones(1), A)
-
-    for k in range(n - 1):
-        A[k+1:, k] = A[k+1:, k] / A[k, k]
-        A[k+1:, k+1:] = A[k+1:, k+1:] - np.outer(A[k+1:, k], A[k, k+1:])
-    
-    L: np.ndarray = np.tril(A, -1)
-    U: np.ndarray = np.triu(A, 0)
     for i in range(n):
-        L[i, i] = 1.0
+        pivot = A[i, i]
+        # check if the pivot is non-zero, equivalent to A is invertible.
+        if abs(pivot) <= 1e-8:
+            raise ZeroDivisionError("pivot should not be zero")
+        A[i+1:, i:i+1] = A[i+1:, i:i+1] / pivot
+        A[i+1:, i+1:] = A[i+1:, i+1:] - A[i+1:, i:i+1] @ A[i:i+1, i+1:]
+    
+    L: np.ndarray = np.eye(n) + np.tril(A, -1)
+    U: np.ndarray = np.triu(A)
 
     return (L, U)
 
--- LU_decomposition_test.py
+++ LU_decomposition_test.py
@@ -2,29 +2,26 @@ import scipy.linalg as la
 import unittest
 import numpy as np
 
-from .LU_decomposition import out_product_lu
+from LU_decomposition import out_product_lu
 
 class TestLUDecomposition(unittest.TestCase):
     def setUp(self):
-        self.A = np.random.randint(100, size=(5, 5))
-        print(self.A)
+        self.A = np.array([[ -50.81816222,   17.50242678,  -18.5187156,   104.48252865,  -35.6882816 ],
+                            [  31.31130212,  -80.91854366, -34.54258943, -127.90181121,    1.88355938],
+                            [ -15.0798181,   248.42511054,   77.33964127,   41.61896562,   10.75753459],
+                            [-114.65834707,    0.84312809,   22.4641862,   -53.75328591, -152.18389341],
+                            [ -71.75615361,  -80.83956885, -166.6450018,   142.40901914,   74.17852845]])
 
     def test1(self):
-        test_L, test_U = out_product_lu(self.A)
-        print(self.A)
-        print(test_L @ test_U)
-        self.assertTrue(np.allclose(test_L @ test_U, self.A))
+        B = self.A.copy()
+        test_L, test_U = out_product_lu(B)
+        np.testing.assert_array_almost_equal(test_L @ test_U, self.A, decimal=3)
 
     def test2(self):
         P, real_L, real_U = la.lu(self.A)
         test_L, test_U = out_product_lu(np.dot(P.T, self.A))
-
-        print(test_L)
-        print(real_L)
-        print(test_U)
-        print(real_U)
-        self.assertTrue(np.allclose(real_L, test_L))
-        self.assertTrue(np.allclose(real_U, test_U))
+        np.testing.assert_array_almost_equal(real_L, test_L, decimal=3)
+        np.testing.assert_array_almost_equal(real_U, test_U, decimal=3)
 
 
 
