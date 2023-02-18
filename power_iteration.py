045c09022e87c00c71c34136613fef87fc9b4b2a
MaoSong2022 <maosong@pjlab.org.cn>
2023-02-18 20:56:53 +0800

    feat(power iterations): add the implementations of power iteration

---
+++ power_iteration.py
@@ -0,0 +1,77 @@
+import numpy as np
+import scipy
+from typing import Tuple
+
+
+def power_iteration(A: np.ndarray, max_iterations: int = 100,
+                    v: np.ndarray = None) -> Tuple[float, np.ndarray]:
+    """Find the maximum eigenvalue and corresponding eigenvector
+
+    Args:
+        A (np.ndarray): an n-by-n matrix
+        max_iterations (int, optional): maximum iterations. Defaults to 100.
+        v (np.ndarray): initial guess. Defaults to None.
+
+    Returns:
+        Tuple[float, np.ndarray]: the maximum eigenvalue and corresponding eigenvector.
+    """
+    m, n = A.shape
+    if m != n:
+        raise ValueError("Power iteration only supports square matrix.")
+
+    if v is None:
+        v = np.random.rand(n)
+
+    for _ in range(max_iterations):
+        v = A @ v
+        norm_v = np.linalg.norm(v)
+        v /= norm_v
+        lamb = v.T @ A @ v
+
+    return lamb, v
+
+
+def rayleigh_quotient_iteration(A: np.ndarray, max_iterations: int = 100,
+                                v: np.ndarray = None) -> Tuple[float, np.ndarray]:
+    
+    m, n = A.shape
+    if m != n:
+        raise ValueError("Rayleigh quotient iteration only supports square matrix.")
+
+    if v is None:
+        v = np.random.randn(n)
+    elif v.ndim != 1:
+        raise ValueError("The initialization vector should be 1 dimensional.")
+
+    v = v / np.linalg.norm(v)
+    lamb: float = v.T @ A @ v
+
+    for _ in range(max_iterations):
+        v = np.linalg.inv(A - lamb * np.eye(n)) @ v
+        v /= np.linalg.norm(v)
+        lamb = v.T @ A @ v
+
+        # residual = A @ v - lamb * v
+
+    return lamb, v
+
+
+def orthogonal_iteration(A: np.ndarray, k: int = 1, max_iterations: int = 100,
+                         V: np.ndarray = None) -> Tuple[np.ndarray, np.ndarray]:
+    m, n = A.shape
+    if m != n:
+        raise ValueError("Orthogonal iteration only supports square matrix.")
+    
+    if V is not None:
+        if V.shape != (n, k):
+            raise ValueError("The shape of initialization space does not match the input.")
+    else:
+        V = np.random.randn(n, k)
+
+    for i in range(max_iterations):
+        Q, R = np.linalg.qr(V, 'reduced')
+        Q /= np.linalg.norm(Q, axis=0)
+        V = A @ Q
+
+        # residual = V - Q @ eigs
+    return (Q, R)
---
+++ power_iteration_test.py
@@ -0,0 +1,37 @@
+import unittest
+import numpy as np
+
+from power_iteration import power_iteration, rayleigh_quotient_iteration, orthogonal_iteration
+
+class TestPowerIterations(unittest.TestCase):
+    def setUp(self):
+        n = 5
+        eig_vectors = np.random.randn(n, n)
+        eig_vectors /= np.linalg.norm(eig_vectors, axis=0)
+        eig_values = np.sort(np.random.rand(n))
+        self.A = eig_vectors @ np.diag(eig_values) @ np.linalg.inv(eig_vectors)
+        self.real_lamb = eig_values[-1]
+        self.real_eigenvector = eig_vectors[:, -1]
+
+    def test_power_iterations(self):
+        test_lamb, test_eigenvector = power_iteration(self.A, max_iterations=1000)
+        test_eigenvector /= np.linalg.norm(test_eigenvector)
+        self.assertAlmostEqual(self.real_lamb, test_lamb, places=5)
+        np.testing.assert_array_almost_equal(self.real_eigenvector, test_eigenvector)
+
+    def test_rayleigh_quotient_iterations(self):
+        # remark: the result depends on the initialization.
+        v = self.real_eigenvector
+        test_lamb, test_eigenvector = rayleigh_quotient_iteration(self.A, max_iterations=1000, v=v)
+        self.assertAlmostEqual(self.real_lamb, test_lamb, places=5)
+        np.testing.assert_array_almost_equal(self.real_eigenvector, test_eigenvector)
+    
+    def test_orthogonal_iterations(self):
+        Q, sigma = orthogonal_iteration(self.A, k=5, max_iterations=1000)
+        np.testing.assert_array_almost_equal(self.A, Q @ sigma @ Q.T)
+
+
+if __name__ == '__main__':
+    # print the numpy array in float format
+    np.set_printoptions(suppress=True)
+    unittest.main()
