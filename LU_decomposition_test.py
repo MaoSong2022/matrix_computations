2d7d2a6115e77ed3b7cfe36e11976905a0092e82
MaoSong2022 <maosong@pjlab.org.cn>
2023-02-21 17:28:55 +0800

    🦄 refactor(LU_decompositions_test): move variables to setUp

--- LU_decomposition_test.py
+++ LU_decomposition_test.py
@@ -11,31 +11,35 @@ class TestLUDecomposition(unittest.TestCase):
                            [ -15.0798181,   248.42511054,   77.33964127,   41.61896562,   10.75753459],
                            [-114.65834707,    0.84312809,   22.4641862,   -53.75328591, -152.18389341],
                            [ -71.75615361,  -80.83956885, -166.6450018,   142.40901914,   74.17852845]])
+        self.P, self.real_L, self.real_U = la.lu(self.A)
+        self.B = self.A.copy()
 
     def test_out_product1(self):
-        B = self.A.copy()
-        test_L, test_U = out_product_lu(B)
-        np.testing.assert_array_almost_equal(test_L @ test_U, self.A, decimal=3)
+        test_L, test_U = out_product_lu(self.P.T @ self.B)
+        np.testing.assert_array_almost_equal(test_L @ test_U, self.P.T @ self.A, decimal=3)
+        np.testing.assert_array_almost_equal(self.real_L, test_L, decimal=3)
+        np.testing.assert_array_almost_equal(self.real_U, test_U, decimal=3)
 
     def test_out_product2(self):
-        B = self.A.copy()
-        P, real_L, real_U = la.lu(B)
-        test_L, test_U = out_product_lu(np.dot(P.T, B))
-        np.testing.assert_array_almost_equal(real_L, test_L, decimal=3)
-        np.testing.assert_array_almost_equal(real_U, test_U, decimal=3)
+        test_L, test_U = out_product_lu(self.P.T @ self.B)
+        np.testing.assert_array_almost_equal(test_L @ test_U, self.P.T @ self.A, decimal=3)
+        np.testing.assert_array_almost_equal(self.real_L, test_L, decimal=3)
+        np.testing.assert_array_almost_equal(self.real_U, test_U, decimal=3)
 
 
     def test_gaussian1(self):
-        B = self.A.copy()
-        test_L, test_U = gaussian_lu(B)
-        np.testing.assert_array_almost_equal(test_L @ test_U, self.A, decimal=3)
+        test_L, test_U = gaussian_lu(self.P.T @ self.B)
+        np.testing.assert_array_almost_equal(test_L @ test_U, self.P.T @ self.A, decimal=3)
+        np.testing.assert_array_almost_equal(self.real_L, test_L, decimal=3)
+        np.testing.assert_array_almost_equal(self.real_U, test_U, decimal=3)
 
     def test_gaussian2(self):
-        B = self.A.copy()
-        P, real_L, real_U = la.lu(self.A)
-        test_L, test_U = gaussian_lu(np.dot(P.T, B))
-        np.testing.assert_array_almost_equal(real_L, test_L, decimal=3)
-        np.testing.assert_array_almost_equal(real_U, test_U, decimal=3)
+        test_L, test_U = gaussian_lu(self.P.T @ self.B)
+        np.testing.assert_array_almost_equal(test_L @ test_U, self.P.T @ self.A, decimal=3)
+        np.testing.assert_array_almost_equal(self.real_L, test_L, decimal=3)
+        np.testing.assert_array_almost_equal(self.real_U, test_U, decimal=3)
+
+
 
 if __name__ == '__main__':
     unittest.main()
