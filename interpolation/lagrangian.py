27c69f24f25afceb9680263c385a23afde747d8c
niladmiran <maosong@pjlab.org.cn>
2023-11-23 17:48:16 +0800

    feat(lagrangian.py): add Lagrangian interpolation

---
+++ interpolation/lagrangian.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+def lagrangian_interpolation(data: np.ndarray, x: np.ndarray) -> np.ndarray:
+    """
+    Generates the Lagrangian interpolation for a set of data points.
+
+    Parameters:
+    - data (np.ndarray): An array of shape (n, 2) representing the data points.
+    - x (np.ndarray): An array of shape (m,) representing the points to interpolate.
+
+    Returns:
+    - np.ndarray: An array of shape (m,) representing the interpolated values at the given points.
+    """
+    result = np.zeros_like(x)
+    n = data.shape[0]
+    for i in range(n):
+        numerator, dominator = 1.0, 1.0
+        for j in range(n):
+            if j == i:
+                continue
+            numerator = numerator * (x - data[j, 0])
+            dominator = dominator * (data[i, 0] - data[j, 0])
+
+        L_i = numerator / dominator
+        result = result + data[i, 1] * L_i
+
+    return result
